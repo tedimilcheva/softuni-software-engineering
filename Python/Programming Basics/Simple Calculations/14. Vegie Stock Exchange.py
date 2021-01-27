a46a40c7550a9219539dcc6f2886d7e6a7162b15
TeodoraMilcheva <34985298+tedimilcheva@users.noreply.github.com>
2021-01-28 00:41:29 +0200

    Create 14. Vegie Stock Exchange.py

---
+++ Python/Programming Basics/Simple Calculations/14. Vegie Stock Exchange.py
@@ -0,0 +1,7 @@
+vegie_price = float(input())
+fruit_price = float(input())
+vegie_amount = float(input())
+fruit_amount = float(input())
+euro = 1.94
+
+print((vegie_amount * vegie_price + fruit_amount * fruit_price)/euro)
