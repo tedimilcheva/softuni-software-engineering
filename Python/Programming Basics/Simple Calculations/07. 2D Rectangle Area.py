0404bd5d2b90eee8447d44bad34e52064812558f
TeodoraMilcheva <34985298+tedimilcheva@users.noreply.github.com>
2021-01-28 00:23:14 +0200

    Create 07. 2D Rectangle Area.py

---
+++ Python/Programming Basics/Simple Calculations/07. 2D Rectangle Area.py
@@ -0,0 +1,12 @@
+x1 = float(input())
+y1 = float(input())
+x2 = float(input())
+y2 = float(input())
+
+area = (abs(x1 - x2) * abs(y1 - y2))
+
+circumference = (2 * abs(x1 - x2) + 2 * abs(y1 - y2))
+
+
+print(area)
+print(circumference)
