c9622f7f0df79827eb53e811999ae9dfeff67c21
Teodora Milcheva <34985298+teodoramilcheva@users.noreply.github.com>
2021-02-10 10:42:22 +0200

    Create 09. Vowels Sum.py

---
+++ Python/Programming Basics/Simple Loops/09. Vowels Sum.py
@@ -0,0 +1,17 @@
+str = input()
+total = 0
+
+for letter in str:
+
+    if letter == 'a':
+        total = total + 1
+    elif letter == 'e':
+        total = total + 2
+    elif letter == 'i':
+        total = total + 3
+    elif letter == 'o':
+        total = total + 4
+    elif letter == 'u':
+        total = total + 5
+
+print(total)
