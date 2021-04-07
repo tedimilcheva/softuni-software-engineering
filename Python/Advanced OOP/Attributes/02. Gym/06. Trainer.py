f5e832767adcf84d145a4db0b52c7d4dea572d1a
Teodora Milcheva <34985298+teodoramilcheva@users.noreply.github.com>
2021-04-07 21:50:47 +0300

    Add files via upload

---
+++ Python/Advanced OOP/Attributes/02. Gym/06. Trainer.py
@@ -0,0 +1,17 @@
+class Trainer:
+    id = 0
+
+    @staticmethod
+    def get_next_id():
+        Trainer.id += 1
+        return Trainer.id
+
+    def __init__(self, name: str):
+        self.name = name
+        self.id = self.get_next_id()
+
+    def __repr__(self):
+        return f'Trainer <{self.id}> {self.name}'
+
+
+
